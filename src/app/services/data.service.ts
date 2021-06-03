58554550f06c454dee292b93e900d39bb090d635
alejandrogonzalezalonso <alejandro.gonzalez.7e3@itb.cat>
2021-06-03 18:35:27 +0200

    cambiada la url de dataservice

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,21 +12,23 @@ export class DataService {
 
   async getMotos(){
     //const respuesta = await fetch("http://motos.puigverd.org/motos");
-    const respuesta = await fetch("http://localhost:3000/miapi/getmotos");
+    const respuesta = await fetch("http://alejandro-gonzalez-7e3.alwaysdata.net/miapi/getMotos");
     this.motos = await respuesta.json();
     console.log(this.motos);
+    console.log("hola");
+    
     return this.motos;
     
   }  
 
   async filtro(marca: string){
-    const respuesta = await fetch(`http://localhost:3000/miapi/getmotos?marca=${marca}`);
+    const respuesta = await fetch(`http://alejandro-gonzalez-7e3.alwaysdata.net/miapi/getMotos?marca=${marca}`);
     this.motos = await respuesta.json();
     console.log(this.motos);
     return this.motos;
   }
   async delete(id: number){
-    const respuesta = `http://localhost:3000/miapi/delete/${id}`;
+    const respuesta = `http://alejandro-gonzalez-7e3.alwaysdata.net/miapi/delete/${id}`;
     fetch (respuesta,{
       "method" : "DELETE",
     }).then(response => (console.log(response)))
